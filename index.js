c72618cf3433f56e3a9802942eb4695d0cc27f98
jainiresh <78695432+jainiresh@users.noreply.github.com>
2023-11-07 00:28:35 +0530

    Update index.js
    updated it

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 // Import the dotenv package to load environment variables
 require('dotenv').config();
 
+// Import the Rookout SDK
+const rookout = require('rookout');
+
 // Access your environment variables
 const rookoutToken = process.env.ROOKOUT_TOKEN;
 const rookoutLabels = process.env.ROOKOUT_LABELS;
+
+// Initialize the Rookout SDK with your token
+rookout.start({ token: rookoutToken });
 
 // Your Node.js application code goes here
 // ...
@@ -11,4 +17,3 @@ const rookoutLabels = process.env.ROOKOUT_LABELS;
 // Example: Print the loaded environment variables
 console.log("ROOKOUT_TOKEN:", rookoutToken);
 console.log("ROOKOUT_LABELS:", rookoutLabels);
-
